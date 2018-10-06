0e34090e8fd7b23f70c1281979ecef210d57e63c
Erfan Alimohammadi <erfan.aa@gmail.com>
2018-10-06 19:44:34 +0330

    Update files

---
+++ credentials.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+SPOTIFY_CLIENT_ID = ''
+SPOTIFY_CLIENT_SECRET = ''
+
+SPOTIFY_PLAYLIST_OWNER = 'spotifycharts'
+SPOTIFY_PLAYLIST_ID = '37i9dQZEVXbMDoHDwVN2tF'
+
+TWITTER_SEARCH_QUERIES = ['"آهنگ معرفی کنید"', '"آهنگ پیشنهاد کنید"', '"آهنگ جدید چی اومده"', '"آهنگ معرفی کن"', '"آهنگ خوب بگید"', '"آهنگ خوب بگو"', '"آهنگ جدید بگید"', '"آهنگ جدید بگو"', '"آهنگ چی گوش بدم"', '"آهنگ چی گوش کنم"', '"آهنگ بفرستید برام"', '"آهنگ جدید میخوام"', '"آهنگ بگید گوش کنم"', '"آهنگ بدید گوش کنم"', '"آهنگ خوب بفرستید"', '"آهنگ خوب میخوام"', '"دنبال آهنگ خوبم"', '"دنبال یه آهنگ خوبم"']
+TWITTER_SEARCH_QUERIES += ['"i want some good songs"', '"suggest me a song"', '"suggest me some songs"', '"recommend me a song"', '"recommend me some songs"', '"recommend some songs"', '"suggest some songs"', '"suggest some good songs"', '"recommend some good songs"', '"suggest a good song"', '"recommend a good song"']
+
+BOT_SCREEN_NAME = 'Tweeterify'
